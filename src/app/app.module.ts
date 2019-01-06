349e856637995f9eff67c7dd0285a219603680a3
Alylson Monteiro <alylson@gmail.com>
2019-01-06 20:27:37 -0200

    Remoção do lock e adicionado modulos para trabalhar com lazy load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
-import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FooterComponent,
-    HomeComponent,
-    UsuarioComponent
   ],
   imports: [
     BrowserModule,
